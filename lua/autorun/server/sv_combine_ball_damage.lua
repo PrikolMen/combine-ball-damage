2203546ee775f93d2cae6c1f5c44a0fd584c91e0
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-03-10 23:26:18 +0400

    +Attacker

--- lua/autorun/server/sv_combine_ball_damage.lua
+++ lua/autorun/server/sv_combine_ball_damage.lua
@@ -2,12 +2,20 @@ local class = "prop_combine_ball"
 local timer_Simple = timer.Simple
 local IsValid = IsValid
 
-hook.Add("OnEntityCreated", class .. "_owner_damage", function(ent)
+local hookName = class .. "_owner_damage"
+hook.Add("OnEntityCreated", hookName, function( ent )
     if (ent:GetClass() == class) then
         timer_Simple(0, function()
             if IsValid( ent ) then
+                ent.Attacker = ent:GetOwner()
                 ent:SetOwner()
             end
         end)
+    end
+end)
+
+hook.Add("EntityTakeDamage", hookName, function( ent, dmg )
+    if (ent:GetClass() == class) and IsValid( ent.Attacker ) then
+        dmg:SetAttacker( ent.Attacker )
     end
 end)
