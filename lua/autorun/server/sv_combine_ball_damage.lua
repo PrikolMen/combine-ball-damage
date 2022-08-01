168acb5a5dbbd691452f28eb9b3a2957bf8c6991
PrikolMen <olegis019@gmail.com>
2022-08-01 20:19:36 +0400

    impulse & npc fix

--- lua/autorun/server/sv_combine_ball_damage.lua
+++ lua/autorun/server/sv_combine_ball_damage.lua
@@ -1,4 +1,5 @@
 local timer_Simple = timer.Simple
+local math_floor = math.floor
 local IsValid = IsValid
 
 local class = "prop_combine_ball"
@@ -6,17 +7,21 @@ local hookName = class .. "_owner_damage"
 
 hook.Add("OnEntityCreated", hookName, function( ent )
     if (ent:GetClass() == class) then
-        local owner = nil
         timer_Simple(0, function()
             if IsValid( ent ) then
-                owner = ent:GetOwner()
-                ent:SetOwner()
-            end
-        end)
+                local att = ent:GetOwner()
+                if IsValid( att ) and att:IsPlayer() then
+                    local owner = ent:GetOwner()
+                    ent:SetOwner()
 
-        timer_Simple(0.05, function()
-            if IsValid( ent ) and IsValid( owner ) then
-                owner:SetVelocity( -ent:GetVelocity() )
+                    timer_Simple(0.05, function()
+                        if IsValid( ent ) and IsValid( owner ) then
+                            local vel = -ent:GetVelocity() / 3
+                            vel[3] = math_floor( vel[3] / 2 )
+                            owner:SetVelocity( vel )
+                        end
+                    end)
+                end
             end
         end)
     end
