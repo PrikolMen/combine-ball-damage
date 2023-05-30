4d0b8558e74df663b4fada1b0dd2ad569ef972b0
PrikolMen <olegis019@gmail.com>
2023-05-30 20:49:49 +0400

    Updated

--- lua/autorun/server/sv_combine_ball_damage.lua
+++ lua/autorun/server/sv_combine_ball_damage.lua
@@ -1,32 +1,42 @@
 local timer_Simple = timer.Simple
 local IsValid = IsValid
 
-local addonName = 'Combine Ball Damage'
-local combineBallClass = 'prop_combine_ball'
+local addonName = "Combine Ball Damage"
+local combineBallClass = "prop_combine_ball"
 
-hook.Add( 'OnEntityCreated', addonName, function( ent )
-    if ent:GetClass() ~= combineBallClass then return end
+hook.Add( "OnEntityCreated", addonName, function( entity )
+    if entity:GetClass() ~= combineBallClass then return end
 
     timer_Simple( 0, function()
-        if not IsValid( ent ) then return end
-
-        local owner = ent:GetOwner()
-        if IsValid( owner ) and owner:IsPlayer() then
-            ent:SetOwner()
+        if not IsValid( entity ) then return end
+        local ply = entity:GetOwner()
+        if IsValid( ply ) and ply:IsPlayer() then
+            entity[ addonName ] = ply
+            entity:SetOwner()
         end
     end )
 end )
 
-local ballDamage = CreateConVar( 'sv_combine_ball_damage', '10000', FCVAR_ARCHIVE, 'Sets the damage value for ' .. combineBallClass .. '.', 15, 1000000 )
+local ballDamage = CreateConVar( "sv_combine_ball_damage", "10000", FCVAR_ARCHIVE, "Sets the damage value for '" .. combineBallClass .. "'.", 15, 1000000 )
 local DMG_DISSOLVE = DMG_DISSOLVE
 local bit_band = bit.band
 
-hook.Add( 'EntityTakeDamage', addonName, function( ent, dmg )
-    if not IsValid( ent ) then return end
-    if bit_band( dmg:GetDamageType(), DMG_DISSOLVE ) ~= DMG_DISSOLVE then return end
+hook.Add( "EntityTakeDamage", addonName, function( _, damageInfo )
+    if bit_band( damageInfo:GetDamageType(), DMG_DISSOLVE ) ~= DMG_DISSOLVE then return end
 
-    local infl = dmg:GetInflictor()
-    if IsValid( infl ) and infl:GetClass() == combineBallClass then
-        dmg:SetDamage( ballDamage:GetInt() )
+    local entity = damageInfo:GetInflictor()
+    if IsValid( entity ) and entity:GetClass() == combineBallClass then
+        local ply = entity[ addonName ]
+        if IsValid( ply ) then
+            timer_Simple( 0, function()
+                if not IsValid( entity ) then return end
+                entity:SetOwner()
+            end )
+
+            damageInfo:SetAttacker( ply )
+            entity:SetOwner( ply )
+        end
+
+        damageInfo:SetDamage( ballDamage:GetInt() )
     end
 end )
